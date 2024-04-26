4a6f830792e4f54a23e50c858a37c49cc6b1a219
@VinayJojare <145494575+VinayJojare@users.noreply.github.com>
2024-04-27 00:10:49 +0530

    Detail.jsx

--- react-firebase-chat/src/components/detail/Detail.jsx
+++ react-firebase-chat/src/components/detail/Detail.jsx
@@ -31,74 +31,17 @@ const Detail = () => {
 
     return (
         <div className='detail'>
-            <div className="user">
-                <img src={user?.avatar || "./avatar.png"} alt=""></img>
-                <h2>{user?.username}</h2>
-                <p>user details</p>
-            </div>
-            <div className="info">
-                <div className="option">
-                    <div className="title">
-                        <span>Chat settings</span>
-                        <img src="./arrowDown.png" alt=""></img>
-                    </div>
-                </div>
-
-                <div className="option">
-                    <div className="title">
-                        <span>Chat settings</span>
-                        <img src="./arrowUp.png" alt=""></img>
-                    </div>
-                </div>
-
-                <div className="option">
-                    <div className="title">
-                        <span>Privacy & help</span>
-                        <img src="./arrowUp.png" alt=""></img>
-                    </div>
-                </div>
-
-                <div className="option">
-                    <div className="title">
-                        <span>Shared Photos</span>
-                        <img src="./arrowDown.png" alt=""></img>
-                    </div>
-                </div>
-                <div className="photos">
-                    
-
-
-                    <div className="photoItem">
-                        <div className="photoDetail">
-
-                   <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"></img>
-                        <span>Photo_abc_554.png</span>
-                        </div>
-                    <img src="./download.png" alt="" className="icon"></img>
-                    </div>
-
-                    <div className="photoItem">
-                        <div className="photoDetail">
-
-                   <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"></img>
-                        <span>Photo_abc_554.png</span>
-                        </div>
-                    <img src="./download.png" alt="" className="icon"></img>
-                    </div>
-                </div>
-
-                <div className="option">
-                    <div className="title">
-                        <span>Shared Files</span>
-                        <img src="./arrowDown.png" alt=""></img>
-                    </div>
-                </div>
-                <button onClick={handleBlock}>
-                    {isCurrentUserBlocked ? "You are Blocked!" : isReceiverBlocked ? "User Blocked" : "Block User!"}
-                </button>
-                <button className="logout" onClick={()=>auth.signOut()}>Logout</button>
-            </div>
+        <div className="user">
+            <img src={user?.avatar || "./avatar.png"} alt=""></img>
+            <h2>{user?.username}</h2>
+            <p>user details</p>
         </div>
+        <button onClick={handleBlock}>
+            {isCurrentUserBlocked ? "You are Blocked!" : isReceiverBlocked ? "User Blocked" : "Block User!"}
+        </button>
+        <br/>
+        <button className="logout" onClick={()=>auth.signOut()}>Logout</button>
+    </div>
     )
 }
 
